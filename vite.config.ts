d773c5f202a4193ffa29c8e6c96b62e6eab916b6
Buildify <buildify-bot@buildify.dev>
2026-07-17 01:37:10 +0000

    Updated project files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  define: {
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('https://zeouviazhsmeguhprjwh.supabase.co'),
+    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inplb3V2aWF6aHNtZWd1aHByandoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMzkxNzcsImV4cCI6MjA5OTgxNTE3N30.bLfSQzfIAVP0XGx64kfTp69ccBnRjwuMMHm9Co3gLSg'),
+  },
   server: {
     allowedHosts: true,
     host: "0.0.0.0",
